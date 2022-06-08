64e8c422d38c7ea71d6382e9f46840b55dff1fe0
Mathias Ege Pontoppidan Clausen <matcla@pc-tec.local>
2022-06-08 09:00:03 +0200

    a

--- PHP/loginAccount.php
+++ PHP/loginAccount.php
@@ -37,7 +37,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         // Prepare a select statement
         $sql = "SELECT id, username, password FROM users WHERE username ='$username'";
         
-        if($stmt = mysqli_prepare($link, $sql)){
+        if($stmt = mysqli_prepare($conn, $sql)){
             // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "s", $param_username);
             
@@ -83,7 +83,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         }
     }
     
-    // // Close connection
-    // mysqli_close($link);
+    // Close connection
+    mysqli_close($conn);
 }
 ?>
