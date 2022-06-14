532d9e82e67214638c2932a0af2657acfab6933b
dedtri <30638362+dedtri@users.noreply.github.com>
2022-06-14 10:56:48 +0200

    a

--- html/AdminSite.php
+++ html/AdminSite.php
@@ -12,7 +12,7 @@ $result = mysqli_query($conn, $query);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin site</title>
+    <title>Adminside</title>
     <link rel="stylesheet" href="../css/style.css">
 
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
