b3c26dedb1aa2f6f5d07c106ccd33b822eff049b
mathias clausen <mathiasepc@msn.com>
2022-06-14 10:28:52 +0200

    a

--- html/AdminSite.php
+++ html/AdminSite.php
@@ -1,31 +1,72 @@
 <?php
-require_once('../php/db_connect.php'); // Database connection file
-require_once('../php/buttonCounter.php');  // PHP functions file
+require_once "../php/db_connect.php";
+require_once('../php/buttonCounter.php');
+
+$query = "select * from antalBesøg";
+$result = mysqli_query($conn, $query);
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Chart</title>
     <link rel="stylesheet" href="../css/style.css">
-</head>
 
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+
+          <?php
+
+        while($chart = mysqli_fetch_assoc($result))
+                {
+                    echo "['".$chart['navn']."',".$chart['samlet_besøg']."],";
+                }
+
+          ?>
+
+        ]);
+
+        var options = {
+          title: 'Antal besøg på de bestemte sider'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data);
+      }
+    </script>
+
+</head>
 <body>
 <div class="wrapper" id="wrapper">
+<div class="pageHeader">
+            <a class="logoLink" href="./index.php"><span class="" id="logo">
+                    <p class="logoName">Skoleoplæringscenter</p>
+                </span>
+                <a href="../php/logout.php"><Button class="loginButton">Logout</Button></a>
+    </a>
+        </div>
+        <div class="mainSection">
+        <div class="topContent">
+                <h1>Antal besøg på bestemte sider</h1>
+            </div>
+        <div class="mainContent">
+        <div id="piechart" style="width: 900px; height: 500px;"></div>
+    </div>
+    <div class="mainContent">
 
-<header class="pageHeader">
-    <a style="text-decoration: none;"Class="logoLink" href="./index.php"><p class="logoName">Skoleoplæringscenter</p></a>
-    <a href="../php/logout.php"><Button class="loginButton">Logout</Button></a>
-</header>
-<br>
-
-<p class="ITFagTitle">Du nu logget ind</p>
-<p class="ITFagTitle">
-    
-<br>
-<?php
+    <p class="stats">
+    <?php
 $total_page1_views = total_views($conn, 1); // Returns total views of this page
 echo "<strong>Antal besøg på IT-support:</strong> " . $total_page1_views;
 ?>
@@ -42,18 +83,25 @@ echo "<strong>Antal besøg på Infrastruktur:</strong> " . $total_page2_views;
 <br>
 <?php
 $total_page2_views = total_views($conn, 4); // Returns total views of this page
-echo "<strong>Antal besøg på Drift:</strong> " . $total_page2_views;
+echo "<strong>Antal besøg på Drift/Lab:</strong> " . $total_page2_views;
 ?>  
 <br>
 <?php
 $total_page2_views = total_views($conn, 5); // Returns total views of this page
-echo "<strong>Antal besøg på Lab:</strong> " . $total_page2_views;
+echo "<strong>Antal besøg på IT-linje lokaler:</strong> " . $total_page2_views;
 ?>  
 <br>
 <?php
 $total_page2_views = total_views($conn, 6); // Returns total views of this page
 echo "<strong>Antal besøg på Services:</strong> " . $total_page2_views;
 ?>  
+
+</p>
+
+
+    </div>
+    </div>
+
 </div>
 </body>
 </html>
